45ea7a1ae227cfbf823bd395512cadfab2c79b3d
henryantwi <henryantwi191@gmail.com>
2024-12-12 20:24:20 +0000

    DjangoSettingModule added to asgi app

--- a_core/asgi.py
+++ a_core/asgi.py
@@ -7,6 +7,8 @@ from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+
 from a_rtchat import routing
 
 application = ProtocolTypeRouter(
