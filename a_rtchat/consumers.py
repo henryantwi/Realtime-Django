e10e4b8a36756e8a407e681da18dd8e307709d4d
henryantwi <henryantwi191@gmail.com>
2024-12-12 09:25:51 +0000

    Add online user tracking and update chatroom functionality

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -1,10 +1,14 @@
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
+from icecream import ic
 
 from .models import ChatGroup, GroupMessage
+
+ic.disable()
 
 
 class ChatroomConsumer(WebsocketConsumer):
@@ -12,7 +16,27 @@ class ChatroomConsumer(WebsocketConsumer):
         self.user = self.scope["user"]
         self.chatroom_name = self.scope["url_route"]["kwargs"]["chatroom_name"]
         self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+
+        # We've added a channel to a group
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name, self.channel_name
+        )
+
+        # add and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+
         self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
+        )
+
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -21,8 +45,32 @@ class ChatroomConsumer(WebsocketConsumer):
         message = GroupMessage.objects.create(
             group=self.chatroom, author=self.user, body=body
         )
+        event = {
+            "type": "message_handler",
+            "message_id": message.id,
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+    def message_handler(self, event):
+        message_id = event["message_id"]
+        message = GroupMessage.objects.get(id=message_id)
         context = {"message": message, "user": self.user}
         html = render_to_string(
             "a_rtchat/partials/chat_message_p.html", context=context
         )
+        self.send(text_data=html)
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() - 1
+        ic(online_count)
+        event = {
+            "type": "online_count_handler",
+            "online_count": online_count,
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+    def online_count_handler(self, event):
+        online_count = event["online_count"]
+        context = {"online_count": online_count}
+        html = render_to_string("a_rtchat/partials/online_count.html", context=context)
         self.send(text_data=html)
