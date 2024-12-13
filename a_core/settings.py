be317f81b9105c94e2765b6303e649d41585165c
henryantwi <henryantwi191@gmail.com>
2024-12-13 08:28:13 +0000

    Update ALLOWED_HOSTS for production environment and adjust CSRF_TRUSTED_ORIGINS

--- a_core/settings.py
+++ a_core/settings.py
@@ -12,8 +12,8 @@ ENVIRONMENT = config("ENVIRONMENT", default="development")
 DEBUG = True if ENVIRONMENT == "development" else False
 
 ALLOWED_HOSTS = ['*']
-# if ENVIRONMENT == "production":
-#     ALLOWED_HOSTS = ['realtime-django.onrender.com']
+if ENVIRONMENT == "production":
+    ALLOWED_HOSTS += ['realtime-django.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = ["https://*", "https://realtime-django.onrender.com"]
 
